f8f68dcf49dd0567f47507a34375f1bad8fc0893
贾 <jiajoy2014@163.com>
2014-12-18 21:52:21 +0800

    添加xcode project

---
+++ IOSDemo/IOSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSDemo
+//
+//  Created by 贾 on 14/12/18.
+//  Copyright (c) 2014年 jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSDemo/IOSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSDemo
+//
+//  Created by 贾 on 14/12/18.
+//  Copyright (c) 2014年 jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
